2c500e7f7ef6fe6f330445d54c9124f76f253c60
Anna Matsulevits <115091575+annamatsulevits@users.noreply.github.com>
2023-07-05 14:40:56 +0200

    Add files via upload

---
+++ configurations.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  2 17:40:44 2022
+
+@author: annamatsulevits
+"""
+
+GPUS = "0"
+
+TRAINED_MODEL = 'path to the trained model'
+
+INPUT = (96, 96, 96)
+
+OUTPUT = (91, 109, 91)
+
+LESIONS = 'path to the lesions'
+
+BATCH_SIZE = 4
+
+
+
+
+
+
+
+
+
